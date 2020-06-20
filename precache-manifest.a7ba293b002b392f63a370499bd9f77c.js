1229f56bb8819a3faa644d10bfa929020e124840
Jyothi <jyothi.lakkavaram@gmail.com>
2020-06-20 17:52:14 +0530

    Updates

---
+++ precache-manifest.a7ba293b002b392f63a370499bd9f77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8db66756e103e694db36ff5dc9a75ef",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "1345398a5c9b709ba214",
+    "url": "/clock/static/css/main.9d24e6f3.chunk.css"
+  },
+  {
+    "revision": "cde2cc0f0e08f5ec05d8",
+    "url": "/clock/static/js/2.9252630b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clock/static/js/2.9252630b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1345398a5c9b709ba214",
+    "url": "/clock/static/js/main.1e5dfa06.chunk.js"
+  },
+  {
+    "revision": "1b396c0a7d827293ffb6",
+    "url": "/clock/static/js/runtime-main.b93107f1.js"
+  }
+]);
